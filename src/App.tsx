c31163ddf777bb62c88f10771ca7a02e573034b9
julianiff <jule_iff@mac.com>
2020-09-02 18:00:55 +0200

    cleanup mess

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 
-import { Button } from "ls-button";
-import { Card } from "ls-card";
-import { Title } from "ls-title";
-import { CurvedBg } from "ls-curved-bg";
 import { Header } from "ls-header";
 
 import "./App.css";
-import { BlogOne } from "./blog-1";
 
 function App() {
   return (
