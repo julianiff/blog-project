40314a8e0b8a063a35cdf85bc4fa679a48404da8
julianiff <jule_iff@mac.com>
2020-09-02 17:59:03 +0200

    add status page

---
+++ src/blog-1.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Title } from "ls-title";
+import { CurvedBg } from "ls-curved-bg";
+
+export const BlogOne = () => {
+  return (
+    <>
+      <Title>Titel</Title>
+      <CurvedBg></CurvedBg>
+    </>
+  );
+};
--- update_ls.sh
+++ update_ls.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yarn add ls-card@latest && yarn add ls-button@latest && yarn add ls-title@latest && yarn
+yarn add ls-card@latest && yarn add ls-button@latest && yarn add ls-title@latest && yarn add ls-curved-bg@latest && yarn add ls-header@latest && yarn
