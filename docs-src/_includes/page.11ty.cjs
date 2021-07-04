7d470391fb2b1782ddf5a1c046e18b1941340969
julianiff <julian.iff@helsana.ch>
2021-07-04 22:11:09 +0200

    b refactore out components

--- docs-src/_includes/page.11ty.cjs
+++ docs-src/_includes/page.11ty.cjs
@@ -13,7 +13,6 @@ module.exports = function (data) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title}</title>
     <link rel="stylesheet" href="${relative(page.url, '/docs.css')}">
-    <link rel="stylesheet"href="${relative(page.url, '/prism-okaidia.css')}"/>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link
