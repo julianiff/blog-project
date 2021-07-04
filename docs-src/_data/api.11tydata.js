0503b8ee11097d25ec725f8f0b09172fedfa4273
julianiff <julian.iff@helsana.ch>
2021-07-04 22:17:45 +0200

    chore: cleanup repo

--- docs-src/_data/api.11tydata.js
+++ docs-src/_data/api.11tydata.js
@@ -7,10 +7,10 @@
 const fs = require('fs');
 
 module.exports = () => {
-  const customElements = JSON.parse(
-    fs.readFileSync('custom-elements.json', 'utf-8')
-  );
-  return {
-    customElements,
-  };
+  // const customElements = JSON.parse(
+  //   fs.readFileSync('custom-elements.json', 'utf-8')
+  // );
+  // return {
+  //   customElements,
+  // };
 };
