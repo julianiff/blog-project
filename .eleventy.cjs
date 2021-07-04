978454f5358ed23db7b0f1c75367a754721c6412
julianiff <julian.iff@helsana.ch>
2021-07-04 23:21:42 +0200

    chore: change package-json scripts

--- .eleventy.cjs
+++ .eleventy.cjs
@@ -1,18 +1,22 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const CleanCSS = require('clean-css');
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
-  eleventyConfig.addPassthroughCopy('docs-src/docs.css');
-  eleventyConfig.addPassthroughCopy('docs-src/.nojekyll');
+  eleventyConfig.addPassthroughCopy('src/docs.css');
+  eleventyConfig.addPassthroughCopy('src/.nojekyll');
   eleventyConfig.addPassthroughCopy('node_modules/es-module-shims');
   eleventyConfig.addPassthroughCopy(
     'node_modules/@webcomponents/webcomponentsjs'
   );
   eleventyConfig.addPassthroughCopy('node_modules/lit/polyfill-support.js');
+  eleventyConfig.addFilter('cssmin', function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
   return {
     dir: {
-      input: 'docs-src',
-      output: 'docs',
+      input: 'src',
+      output: 'public',
     },
     templateExtensionAliases: {
       '11ty.cjs': '11ty.js',
