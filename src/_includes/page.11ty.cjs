5c27ba9d5fd0216a842a5d1be7f3b6548ec780c0
julianiff <julian.iff@helsana.ch>
2021-09-28 10:32:25 +0200

    feat: update head

--- src/_includes/page.11ty.cjs
+++ src/_includes/page.11ty.cjs
@@ -15,16 +15,6 @@ module.exports = function (data) {
     <script async src="/node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-    rel="preload"
-    href="https://fonts.googleapis.com/css2?family=Bitter:wght@100;200;400;700&display=swap"
-    rel="stylesheet"
-    />
-    <link
-    rel="preload"
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;700&display=swap"
-    rel="stylesheet"
-    />
     <script type="importmap">
     {
       "imports": {
@@ -37,6 +27,16 @@ module.exports = function (data) {
     </script>
     <link rel="stylesheet" href="${relative(page.url, '/styling.css')}">
     <link rel="preload" as="style" onload="this.onload=null;this.rel='stylesheet'" href="https://cdn.skypack.dev/-/iff-design-system@v1.8.3-g5H3NAfp5G41b0yFmtM8/dist=es2020,mode=raw/lib/index.css">
+        <link
+    rel="preload"
+    href="https://fonts.googleapis.com/css2?family=Bitter:wght@100;200;400;700&display=swap"
+    rel="stylesheet"
+    />
+    <link
+    rel="preload"
+    href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;400;700&display=swap"
+    rel="stylesheet"
+    />
     <meta name="description" content="Hey, My passion is to create new digital features and products that transform how users achieve their goals.">
     </head>
     <body theme="blue-gradiant">
